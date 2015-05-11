3b72e5d5a4d1ce793af42e84ab8d5a6492c439bc
JjKassai <jkassai211@gmail.com>
2015-05-10 21:40:51 -0400

    Added the initial project files - code just spits out the adc onto the DAC with a linear multiplier

---
+++ main.c
@@ -0,0 +1,133 @@
+/* 
+ * File:   main.c
+ * Author: root
+ *
+ * Created on April 13, 2015, 7:20 PM
+ */
+#define FCY 33333333
+#define increment 10
+#define ADCGAIN 100
+
+unsigned int i=0;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libpic30.h>
+
+
+// DSPIC33FJ128GP802 Configuration Bit Settings
+
+// 'C' source line config statements
+
+#include <xc.h>
+
+// FBS
+#pragma config BWRP = WRPROTECT_OFF     // Boot Segment Write Protect (Boot Segment may be written)
+#pragma config BSS = NO_FLASH           // Boot Segment Program Flash Code Protection (No Boot program Flash segment)
+#pragma config RBS = NO_RAM             // Boot Segment RAM Protection (No Boot RAM)
+
+// FSS
+#pragma config SWRP = WRPROTECT_OFF     // Secure Segment Program Write Protect (Secure segment may be written)
+#pragma config SSS = NO_FLASH           // Secure Segment Program Flash Code Protection (No Secure Segment)
+#pragma config RSS = NO_RAM             // Secure Segment Data RAM Protection (No Secure RAM)
+
+// FGS
+#pragma config GWRP = OFF               // General Code Segment Write Protect (User program memory is not write-protected)
+#pragma config GSS = OFF                // General Segment Code Protection (User program memory is not code-protected)
+
+// FOSCSEL
+#pragma config FNOSC = PRIPLL              // Oscillator Mode (Primary Oscillator (XT, HS, EC))
+#pragma config IESO = ON               // Internal External Switch Over Mode (Start-up device with user-selected oscillator source)
+
+// FOSC
+#pragma config POSCMD = HS              // Primary Oscillator Source (HS Oscillator Mode)
+#pragma config OSCIOFNC = ON            // OSC2 Pin Function (OSC2 pin has digital I/O function)
+#pragma config IOL1WAY = ON             // Peripheral Pin Select Configuration (Allow Only One Re-configuration)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Both Clock Switching and Fail-Safe Clock Monitor are disabled)
+
+// FWDT
+#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler (1:32,768)
+#pragma config WDTPRE = PR128           // WDT Prescaler (1:128)
+#pragma config WINDIS = OFF             // Watchdog Timer Window (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog timer enabled/disabled by user software)
+
+// FPOR
+#pragma config FPWRT = PWR128           // POR Timer Value (128ms)
+#pragma config ALTI2C = OFF             // Alternate I2C  pins (I2C mapped to SDA1/SCL1 pins)
+
+// FICD
+#pragma config ICS = PGD1               // Comm Channel Select (Communicate on PGC1/EMUC1 and PGD1/EMUD1)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG is Disabled)
+
+
+void setup(void);
+
+int main(int argc, char** argv) {
+    setup();
+    while(1)
+    {
+        //PORTA=0xFFFF;
+        //LATBbits.LATB2=AD1CON1bits.DONE;
+        //LATBbits.LATB3=AD1CON1bits.SAMP;
+        //LATBbits.LATB3=1;
+        //LATBbits.LATB3=0;
+        
+        
+        //AD1CON1bits.DONE=0;
+        //DAC1LDAT=ADC1BUF0*5;
+        
+        DAC1LDAT=ADC1BUF0*ADCGAIN+10;
+        
+        
+        //__delay32(5);
+/*       for(i=0;i<0xFFFF-increment;i+=increment)
+        {
+            DAC1LDAT=i;
+        }
+        for(i=0xFFFF; i>increment; i-=increment)
+        {
+            DAC1LDAT=i;
+        }
+        for(i=0x0000; i<0xFFFF-increment; i+=increment)
+        {
+            DAC1LDAT=i;
+        }
+*/      //DAC1LDAT = ADC1BUF0;
+        //__delay_us(10);
+        //DAC1RDAT = rand();
+    
+    }
+    return (EXIT_SUCCESS);
+}
+
+void setup(void)
+{
+    //OSCILLATOR SETUP
+    OSCCON=0b0011001100000000;
+    //CLKDIV=0b0000000000000001;
+    //PLLFBD=0x0000;
+    
+    //AUX OSCILLATOR SETUP
+    ACLKCON=0b0010111100000000;
+    
+    //DAC SETUP
+    DAC1CON=0b1001000000000000;
+    DAC1STAT=0b1000000010000000;
+    DAC1DFLT=0x0000;
+    DAC1LDAT=0xFFFF;
+    DAC1RDAT=0xFFFF;
+    
+    //ADC SETUP
+    AD1CON1bits.ADON=1;         //Turn on
+    AD1CON1bits.AD12B=0;        //10 bit 4 channels
+    AD1CON1bits.SSRC=0b111;     //Auto-convert
+    AD1CON1bits.ASAM=1;         //Auto Sample
+    AD1CON3bits.ADRC=1;         //Try the RC clock
+    AD1CON3bits.SAMC=0b11111;   //Auto Sample Time = 31 TAD
+    AD1CHS0bits.CH0SB=0b00000;  //Channel 0 pos input is AN0
+    AD1CHS0bits.CH0SA=0b00000;  //Channel 0 pos input sample b is AN0
+    
+    //PORT SETUP
+    TRISA=0x0001;           //RA0 is an analog input from the mic
+    TRISB=0x0000;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DSPIC_Function_Gen.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DSPIC_Function_Gen.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dspicfunctiongen.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dspicfunctiongen.x.tar

1f29307f1781c7276788985fa9e042f63dc4fe2b
JjKassai <jkassai211@gmail.com>
2015-05-25 22:50:19 -0400

    Added a sampling calculator spreadsheet and some new code, as well as split the lookup tables out into their own header file

--- build/default/production/main.o.d
+++ build/default/production/main.o.d
@@ -2,4 +2,5 @@ build/default/production/main.o:  \
 main.c  \
 config_bits.h  \
 misc.h  \
- diagnostics.h 
+ diagnostics.h  \
+lookups.h 
--- main.c
+++ main.c
@@ -13,23 +13,23 @@
 #include "config_bits.h"
 #include "misc.h"
 #include "diagnostics.h"
+#include "lookups.h"
 
 //CONSTANT DECLARATION
 #define FCY 33333333
 #define increment 10
-#define ADCGAIN 20 
+#define ADCGAIN 30 
 #define OFFSET 0x0000
-#define SAMPLE_SIZE 2000
+#define SAMPLE_SIZE 10000
 #define MAX_ADC 2^10
 #define MAX_DAC 2^12
 
-static const unsigned int lookup[] = 
-{};
 
 //VARIABLE DECLARATION
 unsigned int i=0;
-unsigned int input[SAMPLE_SIZE];
-unsigned int min_in, max_in;
+signed int input, output;
+//unsigned int min_in, max_in, time;
+float int1;
 
 //PROTOTYPES
 void setup(void);
@@ -38,11 +38,19 @@ int main(int argc, char** argv) {
     setup();
     while(1)
     {
-        for(i=0;i<2000;i++)
-        {
-            input[i]=ADC1BUF0*ADCGAIN;
-            DAC1RDAT=input[i];
+        //T1CONbits.TON=1;           //Turn on the timer
+        for(i=0;i<SAMPLE_SIZE;i++)
+        { 
+            while(!AD1CON1bits.DONE);  
+            AD1CON1bits.DONE=0;         //Manually clear this bit, otherwise hardware won't do so until the next conversion so you'll continue to test true throughout sampling
+            input=ADC1BUF0;
+            int1=input*(lookup[i]*ADCGAIN);
+            output=(signed int) int1;
+            DAC1RDAT=output;
         }
+        //T1CONbits.TON=0;
+        //time=TMR1;
+        //TMR1=0;
     }
     return (EXIT_SUCCESS);
 }
@@ -62,7 +70,7 @@ void setup(void)
     //DAC SETUP
     //DAC1CON=0b1001000000000000;
     DAC1CONbits.DACEN=1;            //Enable the DAC
-    
+    DAC1CONbits.FORM=1;             //Signed integer for math purposes
     //DAC1STAT=0b1000000010000000;
     DAC1STATbits.LOEN=1;            //Left channel output enable
     DAC1STATbits.ROEN=1;            //Right channel output enable
@@ -75,10 +83,11 @@ void setup(void)
     //ADC SETUP
     AD1CON1bits.ADON=1;             //Turn on
     AD1CON1bits.AD12B=0;            //10 bit 4 channels
-    AD1CON1bits.FORM=0b00;          //Unsigned Integer ouput format
+    AD1CON1bits.FORM=0b01;          //Signed Integer ouput format for math purposes
     AD1CON1bits.SSRC=0b111;         //Auto-convert
     AD1CON1bits.ASAM=1;             //Auto Sample
-    AD1CON3bits.ADRC=1;             //Try the RC clock
+    AD1CON3bits.ADRC=1;             //Try the RC cloc,
+//    AD1CON3bits.ADCS=0b00111111;    //Divide system clock by 64 to get ADC
     AD1CON3bits.SAMC=0b11111;       //Auto Sample Time = 31 TAD
     AD1CHS0bits.CH0SB=0b00000;      //Channel 0 pos input is AN0
     AD1CHS0bits.CH0SA=0b00000;      //Channel 0 pos input sample b is AN0
@@ -86,4 +95,7 @@ void setup(void)
     //PORT SETUP
     TRISA=0x0001;                   //RA0 is an analog input from the mic
     TRISB=0x0000;
+    
+    //TIMER1 SETUP
+    //Default timer values are good
 }
